966dae4890f5100cedd22795f5654575416412c7
Edwin <edwin.guedez@bigcore.com>
2020-03-28 15:39:31 +0100

    lunh algorithm solved

---
+++ python/luhn/luhn.py
@@ -0,0 +1,25 @@
+import functools 
+import re
+
+class Luhn:
+
+    def __init__(self, card_num):
+        self.card_num = card_num
+        self.is_valid = None
+
+    def valid(self):
+        if self.is_valid: return self.is_valid
+
+        self.is_valid = False
+        
+        match = re.search(r"^\d{2,}$", self.card_num.replace(' ', '') )
+
+        if match:
+            total = 0
+            for i in range(len(match.string)-2, -1, -2):
+                x2 = int(match.string[i] or 0) * 2
+                if x2 > 9: x2 -= 9
+                total += x2 + int(match.string[i+1])
+            if total % 10 == 0: self.is_valid = True
+
+        return self.is_valid  
