e10c3c42cc5ac33636167747417daac15cd45bd4
Edwin <edwin.guedez@bigcore.com>
2020-03-29 17:33:41 +0200

    matrix testing

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,17 +1,28 @@
 class Matrix:
 
     def __init__(self, matrix_string):
-
-        self.matrix=[]
-
-        for row_str in matrix_string.splitlines():
-            row=[]
-            for col_str in row_str.split(' '):
-                row.append(int(col_str))
-            self.matrix.append(row)
+        self.matrix = [row.split() for row in matrix_string.splitlines()]
 
     def row(self, index):
-        return self.matrix[index-1]
+        return [int(col) for col in self.matrix[index-1]]
 
     def column(self, index):
-        return [self.matrix[i][index-1] for i in range(len(self.matrix))]
+        return [int(row[index-1]) for row in self.matrix]
+
+def matrix_1(matrix_string,index):
+    matrix = [row.split() for row in matrix_string.splitlines()]
+    return [int(matrix[i][index-1]) for i in range(len(matrix))]
+    
+def matrix_2(matrix_string,index):
+    matrix = [row.split() for row in matrix_string.splitlines()]
+    return [int(row[index-1]) for row in matrix]
+    
+
+if __name__=='__main__':
+    import timeit
+    
+    #print(matrix_1('1 2 3\n4 5 6\n7 8 9\n8 7 6', 3))
+    #print(matrix_2('1 2 3\n4 5 6\n7 8 9\n8 7 6', 3))
+    
+    print(timeit.repeat(r"matrix_1('1 2 3\n4 5 6\n7 8 9\n8 7 6', 3)", setup='from __main__ import matrix_1', repeat=3, number=100000))
+    print(timeit.repeat(r"matrix_2('1 2 3\n4 5 6\n7 8 9\n8 7 6', 3)", setup='from __main__ import matrix_2', repeat=3, number=100000))
